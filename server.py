504c4f0f369fb7de46245ef04f8c6792217b9f10
cumulus <luck1306@naver.com>
2024-10-12 16:26:14 +0900

    [UPDATE] Saving Server

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template, redirect
+from server_util import search_dc, search_namu
+
+app = Flask("Scrapper")
+
+@app.get("/")
+def home():
+    return render_template("index.html")
+
+@app.get("/find")
+def find():
+    keyword: str = request.args.get("keyword")
+    find_type: str = request.args.get("service_type")
+    site_info: list = []
+
+    if keyword == None or find_type == None: return redirect("/")
+
+    if find_type == "namu":
+        site_info = search_namu()
+        site_info.insert(0, []) # head_list
+    else:
+        site_info = search_dc(keyword)
+
+    return render_template("find.html", service_type=find_type, keyword=keyword, head_list=site_info[0])
+
+app.run("localhost", "8080")
---
+++ server_util.py
@@ -0,0 +1,13 @@
+"""
+return [rankings]
+"""
+def search_namu() -> list:
+    source_url = "https://namu.wiki"
+    return []
+
+"""
+return [[headers], [values]]
+"""
+def search_dc(keyword: str) -> list:
+    source_url = "https://www.dcinside.com"
+    return [[], []]
