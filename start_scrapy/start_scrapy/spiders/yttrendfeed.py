07b3ec679e7dd7e0621f9c5c6afaa1d41767a029
cumulus <luck1306@naver.com>
2024-12-17 00:02:42 +0900

    [UPDATE] Separate Extracting Data to Subroutine

--- start_scrapy/start_scrapy/spiders/yttrendfeed.py
+++ start_scrapy/start_scrapy/spiders/yttrendfeed.py
@@ -1,7 +1,9 @@
 from typing import Iterable
 import scrapy
 from scrapy.http import Request
+from scrapy.selector import Selector
 from start_scrapy.items import YTTitleItem, YTElementItem
+
 
 
 class YttrendfeedSpider(scrapy.Spider):
@@ -13,25 +15,30 @@ class YttrendfeedSpider(scrapy.Spider):
         yield scrapy.Request(self.start_urls[0], meta={"playwright": True})
 
     # ytd-item-section-renderer.style-scope <- section
-
     #grid-container > ytd-video-renderer.style-scope <- typical video box
     #items > .yt-horizontal-list-renderer <- video box 4 shorts
     # ytm-shorts-lockup-view-model > div > h3 > a <- shorts title and link
     #text > a -> video channel info
     def parse(self, response):
-        # null_te = YTTitleItem()
-        # null_te["name"] = "nameless"
+        null_te = YTTitleItem()
+        null_te["name"] = "noname"
+        null_te["elements"] = []
         for section in response.css("ytd-item-section-renderer.style-scope"):
             te = YTTitleItem()
             te["name"] = section.css("h2 span#title::text").get() or ""
             te["elements"] = []
+            yield from self.extract_elements(section, te)
+    def extract_elements(
+        self,
+        section: Selector,
+        te: YTTitleItem
+        ):
             if section.css("ytm-shorts-lockup-view-model-v2").get() is not None: # for shorts
                 for es in section.css("#items > .yt-horizontal-list-renderer"):
                     e = YTElementItem()
-                    e["name"] = es.css("ytm-shorts-lockup-view-model > div > h3 > a::attr('title')").get()
-                    e["url"] = es.css("ytm-shorts-lockup-view-model > div > h3 > a::attr('href')").get()
+                    e["name"] = es.css("ytm-shorts-lockup-view-model > div > h3 > a::attr(title)").get()
+                    e["url"] = es.css("ytm-shorts-lockup-view-model > div > h3 > a::attr(href)").get()
                     te["elements"].append(e)
-                yield te
             else: # typical video
                 for es in section.css("#grid-container > ytd-video-renderer.style-scope"):
                     e = YTElementItem()
@@ -40,4 +47,4 @@ class YttrendfeedSpider(scrapy.Spider):
                     e["channel"] = es.css("#text > a::text").get()
                     e["channel_link"] = es.css("#text > a::attr(href)").get()
                     te["elements"].append(e)
-                yield te
+            yield te
